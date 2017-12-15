6a73942562159c282f40faba1b77cb462ef0fe75
BeAce <beaceshimin@gmail.com>
2017-12-15 14:17:39 +0800

    drop bizchart used recharts

--- app/containers/Dashboard/index.js
+++ app/containers/Dashboard/index.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import { Row, Col, Card, Tooltip, Icon } from 'antd';
-import { Chart, Geom, Axis, Legend } from 'bizcharts';
+import Recharts, { LineChart, Line, XAxis, YAxis, CartesianGrid, Legend } from 'recharts';
 import styles from './index.css';
 
-// 数据源
 const data = [
-  { genre: 'Sports', sold: 275, income: 2300 },
-  { genre: 'Strategy', sold: 115, income: 667 },
-  { genre: 'Action', sold: 120, income: 982 },
-  { genre: 'Shooter', sold: 350, income: 5271 },
-  { genre: 'Other', sold: 150, income: 3710 }
+  { name: 'Page A', uv: 4000, pv: 2400, amt: 2400 },
+  { name: 'Page B', uv: 3000, pv: 1398, amt: 2210 },
+  { name: 'Page C', uv: 2000, pv: 9800, amt: 2290 },
+  { name: 'Page D', uv: 2780, pv: 3908, amt: 2000 },
+  { name: 'Page E', uv: 1890, pv: 4800, amt: 2181 },
+  { name: 'Page F', uv: 2390, pv: 3800, amt: 2500 },
+  { name: 'Page G', uv: 3490, pv: 4300, amt: 2100 },
 ];
 
 // 定义度量
@@ -58,13 +59,15 @@ export default class Home extends Component {
               </Card>
             </Col>
           </Row>
-          <Chart width={600} height={400} data={data} scale={cols}>
-            <Axis name="genre" />
-            <Axis name="sold" />
-            <Legend position="top" dy={-20} />
-            <Tooltip />
-            <Geom type="interval" position="genre*sold" color="genre" />
-          </Chart>
+          <LineChart width={600} height={300} data={data} margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+            <XAxis dataKey="name" />
+            <YAxis />
+            <CartesianGrid strokeDasharray="3 3" />
+            <Recharts.Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{r: 8}} />
+            <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+          </LineChart>
         </div>
       </div>
     );
