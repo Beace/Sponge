3e60cd6ae5d1cd23b0b8d1ca35e9f8c31e7026e9
BeAce <beaceshimin@gmail.com>
2017-12-19 16:13:08 +0800

    upgrade all packages and add create article page

--- app/containers/ArticleListPage/index.js
+++ app/containers/ArticleListPage/index.js
@@ -1,64 +1,102 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Table, Divider } from 'antd';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Table, Divider, Popconfirm, message } from "antd";
 
-import fetch from '../../lib/fetch';
+import fetch from "../../lib/fetch";
 
 class ArticleListPage extends React.Component {
   state = {
     data: [],
-    loading: false,
-  }
+    loading: false
+  };
   componentDidMount() {
     this.setState({
-      loading: true,
+      loading: true
     });
-    fetch.get('articles').then(data => {
-      this.setState({
-        data,
-        loading: false,
-      });
-    }).catch(error => {
-      this.setState({
-        loading: false,
-      });
-      console.log(error);
-    });
+    this.getList();
   }
+
+  getList = () => {
+    fetch
+      .get("articles")
+      .then(data => {
+        this.setState({
+          data,
+          loading: false
+        });
+      })
+      .catch(error => {
+        this.setState({
+          loading: false
+        });
+        console.log(error);
+      });
+  }
+
+  confirm = data => {
+    console.log(data);
+    fetch
+      .post("article_delete", { _id: data._id })
+      .then(data => {
+        message.success("删除成功！");
+        this.getList();
+      })
+      .catch(error => message.error(JSON.stringify(error)));
+  };
+
   render() {
     const { loading, data } = this.state;
-    const columns = [{
-      title: '标题',
-      dataIndex: 'title',
-      key: 'title',
-    }, {
-      title: '作者',
-      dataIndex: 'author',
-      key: 'author',
-      render: text => text || 'Beace',
-    }, {
-      title: '创建时间',
-      dataIndex: 'date',
-      key: 'date',
-    }, {
-      title: '描述',
-      dataIndex: 'abstract',
-      key: 'abstract',
-      width: '20%',
-    }, {
-      title: '操作',
-      key: 'action',
-      render: (text, record) => (
-        <span>
-          <Link to={`/articles/${record._id}`}>编辑</Link>
-          <Divider type="vertical" />
-          <Link to="#">删除</Link>
-        </span>
-      ),
-    }];
+    const columns = [
+      {
+        title: "标题",
+        dataIndex: "title",
+        key: "title"
+      },
+      {
+        title: "作者",
+        dataIndex: "author",
+        key: "author",
+        render: text => text || "Beace"
+      },
+      {
+        title: "创建时间",
+        dataIndex: "date",
+        key: "date"
+      },
+      {
+        title: "描述",
+        dataIndex: "abstract",
+        key: "abstract",
+        width: "20%"
+      },
+      {
+        title: "操作",
+        key: "action",
+        render: (text, record) => (
+          <span>
+            <Link to={`/articles/${record._id}`}>编辑</Link>
+            <Divider type="vertical" />
+            <Popconfirm
+              placement="topLeft"
+              title="确认删除该记录？"
+              onConfirm={() => this.confirm(record)}
+              okText="删除"
+              cancelText="取消"
+            >
+              <a>删除</a>
+            </Popconfirm>
+          </span>
+        )
+      }
+    ];
     return (
       <div>
-        <Table loading={loading} columns={columns} dataSource={data} rowKey="_id" />
+        <Table
+          loading={loading}
+          columns={columns}
+          dataSource={data}
+          rowKey="_id"
+        />
       </div>
     );
   }
--- app/routes.js
+++ app/routes.js
@@ -5,6 +5,7 @@ import Layout from "./containers/Layout";
 import Dashboard from "./containers/Dashboard";
 import ArticleListPage from "./containers/ArticleListPage";
 import ArticleDetailPage from "./containers/ArticleDetailPage";
+import ArticleAddPage from "./containers/ArticleAddPage";
 
 export default () => (
   <Layout>
@@ -12,6 +13,7 @@ export default () => (
       <Route exact path="/" component={Dashboard} />
       <Route exact path="/articles" component={ArticleListPage} />
       <Route exact path="/articles/:id" component={ArticleDetailPage} />
+      <Route exact path="/addarticle" component={ArticleAddPage} />
     </div>
   </Layout>
 );
